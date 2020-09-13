5ae1d3af9347f6537da3035610d220d9b6e66adb
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-09-13 18:29:33 +0200

    Fix navigation bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -9,7 +9,7 @@ import { useHistory } from 'react-router-dom';
 
 
 const NavigationBar: React.FC = () => {
-  const [drawerIsOpen, setDrawerOpen] = useState(true);
+  const [drawerIsOpen, setDrawerOpen] = useState(false);
   const history = useHistory();
   
   return (
