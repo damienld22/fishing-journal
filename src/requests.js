fc00fc7417f37d4c6076754c107ebb7bea026107
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-09-16 17:47:20 +0200

    Update URL API

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://92.222.68.241:3000';
+const API_URL = 'https://241.ip-92-222-68.eu';
 
 export async function createFishCatch(fish) {
 	return axios.post(API_URL + '/fishes', fish, {
