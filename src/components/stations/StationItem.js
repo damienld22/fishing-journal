5ced5f14af3c6d77cd96523bcd5c6d037d3bbe9b
Damien <damien-le-dantec@hotmail.fr>
2021-02-04 22:28:42 +0100

    Add list stations

---
+++ src/components/stations/StationItem.js
@@ -0,0 +1,97 @@
+import React, {useState} from 'react';
+import {Card, CardContent} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import TrashIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import {deleteStation} from '../../requests';
+import Snackbar from '../Snackbar';
+import styles from '../components.module.css';
+import FullScreenPicture from '../generics/FullScreenPicture';
+
+const StationItem = ({station, onClick, onDeleteDone, onEdit}) => {
+	const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+	const [messageSnackbar, setMessageSnackbar] = useState(null);
+
+	const handleDelete = () => {
+		setMessageSnackbar(null);
+
+		deleteStation(station._id)
+			.then(() => {
+				setDeleteModalOpen(false);
+				onDeleteDone();
+			})
+			.catch(() => {
+				setMessageSnackbar('Erreur lors de la suppression du poste.');
+			});
+	};
+
+	return (
+		<>
+			<Card raised className={styles.card} onClick={onClick}>
+				<CardContent className={styles.cardContent}>
+					{station.picture ? <FullScreenPicture alt="station" height={50} src={station.picture}/> : <p>/</p>}
+					<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: '0.8em'}}>
+						<p style={{margin: 0}}>{station.distance ? `distance : ${station.distance}m` : '/'}</p>
+						<p style={{margin: 0}}>{station.depth ? `profondeur : ${station.depth}m` : '/'}</p>
+					</div>
+
+					<div>
+						<IconButton
+							style={{margin: 0, padding: 5}} onClick={evt => {
+								evt.preventDefault();
+								evt.stopPropagation();
+								onEdit();
+							}}
+						>
+							<EditIcon/>
+						</IconButton>
+						<IconButton
+							style={{margin: 0, padding: 5}} onClick={evt => {
+								evt.preventDefault();
+								evt.stopPropagation();
+								setDeleteModalOpen(true);
+							}}
+						>
+							<TrashIcon/>
+						</IconButton>
+					</div>
+				</CardContent>
+			</Card>
+
+			<Dialog open={deleteModalOpen} onClose={() => setDeleteModalOpen(false)}>
+				<DialogTitle>Suppression du poste de pêche</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						Voulez-vous réllement supprimer ce poste ?
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button color="primary" onClick={() => setDeleteModalOpen(false)}>
+						Non
+					</Button>
+					<Button autoFocus color="primary" onClick={handleDelete}>
+						Oui
+					</Button>
+				</DialogActions>
+			</Dialog>
+
+			<Snackbar isOpen={Boolean(messageSnackbar)} setState={setMessageSnackbar} message={messageSnackbar}/>
+		</>
+	);
+};
+
+StationItem.propTypes = {
+	station: PropTypes.object,
+	onClick: PropTypes.func.isRequired,
+	onEdit: PropTypes.func.isRequired,
+	onDeleteDone: PropTypes.func.isRequired
+};
+
+export default StationItem;
--- src/pages/ListStations/ListStations.js
+++ src/pages/ListStations/ListStations.js
@@ -1,15 +1,41 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AddIcon from '@material-ui/icons/Add';
-import {IconButton} from '@material-ui/core';
+import {CircularProgress, IconButton, List, Snackbar} from '@material-ui/core';
 import BackIcon from '@material-ui/icons/ArrowBack';
 import {useHistory, useParams} from 'react-router-dom';
 import './ListStations.scss';
 import CreateStationModal from '../../components/stations/CreateStationModal';
+import {getStationsByLocation} from '../../requests';
+import StationItem from '../../components/stations/StationItem';
 
 const ListStations = () => {
 	const {location} = useParams();
 	const history = useHistory();
+	const [stations, setStations] = useState([]);
+	const [selectedStation, setSelectedStation] = useState(null);
+	const [displayProgress, setDisplayProgress] = useState(false);
+	const [messageSnackbar, setMessageSnackbar] = useState(null);
 	const [createStationModalIsOpen, setCreateStationModal] = useState(false);
+
+	function getStationsFromServer() {
+		getStationsByLocation(location)
+			.then(({data}) => {
+				setStations(data);
+				setDisplayProgress(false);
+			})
+			.catch(() => {
+				setMessageSnackbar('Erreur lors de la récupération des postes.');
+				setDisplayProgress(false);
+			});
+	}
+
+	useEffect(() => {
+		if (!createStationModalIsOpen || !selectedStation) {
+			setDisplayProgress(true);
+			getStationsFromServer();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [createStationModalIsOpen, selectedStation]);
 
 	return (
 		<div className="container">
@@ -19,13 +45,26 @@ const ListStations = () => {
 				</IconButton>
 			</div>
 
-			<p>{location}</p>
+			<h1>Zones de pêche</h1>
+
+			<div className="scrollList">
+				<List>
+					{
+						stations.map(station => (
+							<StationItem key={station._id} station={station} onClick={() => history.push('/stations/display', {station})} onEdit={() => setSelectedStation(station)} onDeleteDone={getStationsFromServer}/>
+						))
+					}
+				</List>
+			</div>
 
 			<IconButton className="addButton" onClick={() => setCreateStationModal(true)}>
 				<AddIcon/>
 			</IconButton>
 
+			{ displayProgress && <CircularProgress className="progress"/>}
 			<CreateStationModal isOpen={createStationModalIsOpen} setState={setCreateStationModal} location={location}/>
+			<Snackbar isOpen={Boolean(messageSnackbar)} setState={setMessageSnackbar} message={messageSnackbar}/>
+			{/* {selectedStation && <EditStationModal selectedStation={selectedStation} setSelectedStation={setSelectedStation}/>} */}
 		</div>
 	);
 };
